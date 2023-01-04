4bc12ae65d12b5716c768c88f5d78731f55d488d
leonardofigueiro <leonardo.figueiro@yahoo.com.br>
2023-01-03 21:48:38 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/reset.css'
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import('bootstrap')
+  }, [])
   return <Component {...pageProps} />
 }
