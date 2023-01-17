d6179b98524a51a66a9b2161bbbeefb58b70459c
leonardofigueiro <leonardo.figueiro@yahoo.com.br>
2023-01-16 20:28:20 -0400

    Purge CSS config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -28,4 +23,4 @@ const nextConfig = {
 
 };
 
-module.exports = withBundleAnalyzer({nextConfig});
+module.exports = nextConfig;
